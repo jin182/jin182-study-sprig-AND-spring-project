2f537ec3f1c5c64faa2ed43a66e08701fdf7c40b
Lay182 <jinwoo9288@gmail.com>
2024-09-21 23:07:27 +0900

    cookie를 활용한 로그인 구현 파일 소스코드 위치 수정

--- study(jwt)/cookie/src/main/java/com/example/cookie/controller/UserApiController.java
+++ study(jwt)/cookie/src/main/java/com/example/cookie/controller/UserApiController.java
@@ -1,26 +1,7 @@
 package com.example.cookie.controller;
 
 
-import com.example.cookie.model.LoginRequest;
-import com.example.cookie.service.UserService;
-import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-@RequiredArgsConstructor
-@RestController
-@RequestMapping("api/account")
+
 public class UserApiController {
 
-    private final UserService userService;
-    @PostMapping("/login")
-    public void login(
-        @RequestBody
-        LoginRequest loginRequest,
-        HttpServletResponse httpServletResponse
-    ) {
-        userService.login(loginRequest, httpServletResponse);
-    }
 }
